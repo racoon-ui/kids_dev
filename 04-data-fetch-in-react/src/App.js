63e883aed40dc5d81ce422de69fb129ba79bd4e6
wecanooo <wecanooo@gmail.com>
2020-03-02 14:32:30 +0900

    04-data-fetch-in-react 프로젝트 추가

--- 04-data-fetch-in-react/src/App.js
+++ 04-data-fetch-in-react/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Post = ({ post }) => {
   return (
@@ -17,36 +18,87 @@ const Profile = ({ profile }) => {
   );
 };
 
+// axios.interceptors.request.use(
+//   function(config) {},
+//   function(error) {
+//     return Promise.reject(error);
+//   },
+// );
+
+// axios.interceptors.response.use(
+//   function(response) {},
+//   function(error) {
+//     return Promise.reject(error);
+//   },
+// );
+
 function App() {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
   const [profile, setProfile] = useState(null);
 
-  useEffect(() => {
-    fetch('/posts')
-      .then(res => {
-        return res.json();
-      })
-      .then(json => {
-        setPosts(...posts, json);
-      });
+  /**
+   * 0. library 로써의 차이점을 알아보자 (브라우저 지원, 3rd party library 사용 등)
+   * 1. fetch vs axios 의 차이점을 알아보자
+   * 2. fetch vs axios 의 데이터 가공하는 방식에 대한 차이를 알아보자
+   * 3. error handling 기법에 대해서 알아보자
+   * 4. intercepting 방법이 존재하는지 알아보자.
+   * 5. async function 사용 시 어떤 차이가 있는지 알아보자.
+   * 6. 호출 취소에 대한 방법이 있는지 알아보자.
+   */
 
-    fetch('/profile')
-      .then(res => {
-        return res.json();
-      })
-      .then(json => {
-        setProfile(json);
-      });
+  useEffect(() => {
+    // fetch('/posts')
+    //   .then(res => {
+    //     if (!res.ok) {
+    //       throw Error(res.statusText);
+    //     }
+    //     return res.json();
+    //   })
+    //   .then(json => {
+    //     setPosts(...posts, json);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    //
+    // axios('/posts')
+    //   .then(res => {
+    //     return setPosts(...posts, res.data);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    //
+    // async function fetchPost() {
+    //   try {
+    //     const res = await fetch('/post');
+    //     if (!res.ok) {
+    //       throw Error(res.statusText);
+    //     }
+
+    //     setPosts(await res.json());
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
+    //
+    const fetchPost = async () => {
+      try {
+        const res = await axios('/posts');
+        setPosts(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchPost();
   }, []);
 
   return (
     <div className="App">
       <div className="post-list-container">
-        {posts.length > 0 &&
-          posts.map(post => {
-            return <Post post={post} key={post.id} />;
-          })}
+        {posts.length > 0 && posts.map(post => <Post post={post} key={post.id} />)}
         {posts.length === 0 && <div className="empty-data">데이터가 없습니다.</div>}
       </div>
       {profile && <Profile profile={profile} />}
