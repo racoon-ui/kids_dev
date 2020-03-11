5bab5ac00ffc565aafcddd755a1dd2890ac3bfa5
wecanooo <wecanooo@gmail.com>
2020-03-12 00:22:37 +0900

    1강~10강 코드 연합 완료

--- 11-corona/src/api/corona.ts
+++ 11-corona/src/api/corona.ts
@@ -4,6 +4,7 @@ export async function getPharmacy(address: string) {
   const res = await axios.get<PharmacyProfile>(
     'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json',
   );
+  console.log(res.data);
   return res.data;
 }
 
@@ -11,13 +12,4 @@ export interface PharmacyProfile {
   address: string;
   count: number;
   stores: [];
-  // addr: string;
-  // code: string;
-  // created_at: Date;
-  // lat: number;
-  // lng: number;
-  // name: string;
-  // remain_state: string;
-  // stock_at: Date;
-  // type: string;
 }
--- 11-corona/src/components/Store.tsx
+++ 11-corona/src/components/Store.tsx
@@ -3,14 +3,22 @@ import * as React from 'react';
 type PharmacyProfileInfoProps = {
   name: string;
   addr: string;
-  remain_state: string;
+  remain_stat: string;
   type: string;
   created_at: Date;
   stock_at: Date;
 };
 
-const Store = ({ addr, name, remain_state, created_at, stock_at, type }: PharmacyProfileInfoProps) => {
-  return <div>{name}</div>;
+const Store = ({ addr, name, remain_stat, created_at, stock_at, type }: PharmacyProfileInfoProps) => {
+  return (
+    <React.Fragment>
+      <div>{name}</div>
+      <div>{addr}</div>
+      <div>{remain_stat}</div>
+      <div>{created_at}</div>
+      <div>{stock_at}</div>
+    </React.Fragment>
+  );
 };
 
 export default Store;
