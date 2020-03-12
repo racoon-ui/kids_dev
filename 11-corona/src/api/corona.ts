5373eea529a99c5887b2302251a2ce7e779c74fc
wecanooo <wecanooo@gmail.com>
2020-03-12 10:15:27 +0900

    add address param

--- 11-corona/src/api/corona.ts
+++ 11-corona/src/api/corona.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getPharmacy(address: string) {
   const res = await axios.get<PharmacyProfile>(
-    'https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json',
+    `https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json?address=${address}`,
   );
   return res.data;
 }
